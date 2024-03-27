8d8e4ec95622bbc5304d825a902314eb5f37c43b
Ümit Şimşek <umitsimsek002@gmail.com>
2024-03-27 22:13:25 +0300

    intersection-of-two-arrays

---
+++ LeetCode.playground/Pages/349.intersection-of-two-arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var output = [Int]()
+    var n1 = Set(nums1)
+    var n2 = Set(nums2)
+    
+    for num in n1 {
+        if n2.contains(num) {
+            if !output.contains(num){
+                output.append(num)
+            }
+        }
+    }
+    return output
+}
+intersection([4,9,5], [9,4,9,8,4])
